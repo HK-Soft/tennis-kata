d872091d24e6a56430b68efb92913050aa1ea00b
hadj naasse abdeldjalil <hn.abdeldjalil@gmail.com>
2019-09-18 10:41:15 +0200

    implementation: when one player with 40 score scores

--- src/main/java/kata/tennis/TennisGame.java
+++ src/main/java/kata/tennis/TennisGame.java
@@ -10,10 +10,25 @@ public class TennisGame {
         StandardGameScore result = currentScore.copy();
         if (
             (player == PlayerID.FIRST_PLAYER) &&
+            (currentScore.getFirstPlayerScore() == GameScoreType.FORTY)
+        ){
+            result.setFirstPlayerScore(GameScoreType.WIN);
+            return result;
+        }
+        if (
+            (player == PlayerID.SECOND_PLAYER) &&
+            (currentScore.getSecondPlayerScore() == GameScoreType.FORTY)
+        ){
+            result.setSecondPlayerScore(GameScoreType.WIN);
+            return result;
+        }
+        if (
+            (player == PlayerID.FIRST_PLAYER) &&
             (currentScore.getFirstPlayerScore().ordinal() >= GameScoreType.LOVE.ordinal()) &&
             (currentScore.getFirstPlayerScore().ordinal() < GameScoreType.FORTY.ordinal())
         ) {
             result.setFirstPlayerScore(GameScoreType.values()[currentScore.getFirstPlayerScore().ordinal() + 1]);
+            return result;
         }
         if (
             (player == PlayerID.SECOND_PLAYER) &&
@@ -21,6 +36,7 @@ public class TennisGame {
             (currentScore.getFirstPlayerScore().ordinal() < GameScoreType.FORTY.ordinal())
         ) {
             result.setSecondPlayerScore(GameScoreType.values()[currentScore.getSecondPlayerScore().ordinal() + 1]);
+            return result;
         }
         return result;
     }
