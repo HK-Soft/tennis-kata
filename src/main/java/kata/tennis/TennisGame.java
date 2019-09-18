c75fe680643cecd4deee29c98f3b2a47b1324179
hadj naasse abdeldjalil <hn.abdeldjalil@gmail.com>
2019-09-18 09:40:10 +0200

    first commit

---
+++ src/main/java/kata/tennis/TennisGame.java
@@ -0,0 +1,13 @@
+package kata.tennis;
+
+import java.util.logging.Logger;
+
+public class TennisGame {
+
+    private static final Logger LOGGER = Logger.getLogger(TennisGame.class.getName());
+
+    public static void main(String[] args) {
+        LOGGER.info("Game started");
+    }
+
+}
---
+++ src/test/java/kata/tennis/TestClass.java
@@ -0,0 +1,15 @@
+package kata.tennis;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.logging.Logger;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class TestClass {
+
+    @Test
+    public void alwaysTrue(){
+        assertTrue(true);
+    }
+}
