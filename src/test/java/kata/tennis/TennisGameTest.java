a17b4a81a64cb48cf507de22b30860e6a262b809
hadj naasse abdeldjalil <hn.abdeldjalil@gmail.com>
2019-09-18 18:05:27 +0200

    test: game status after tow scores

--- src/test/java/kata/tennis/TennisGameTest.java
+++ src/test/java/kata/tennis/TennisGameTest.java
@@ -4,7 +4,9 @@ import kata.tennis.scores.SimpleGameScore;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
 import java.util.Collections;
+import java.util.List;
 
 public class TennisGameTest {
 
@@ -19,5 +21,30 @@ public class TennisGameTest {
         Assertions.assertIterableEquals(Collections.singleton(new SimpleGameScore(15, 0)), result.getStatus());
         Assertions.assertIterableEquals(Collections.singleton(new SimpleGameScore(0, 0)), result.getScore());
         Assertions.assertIterableEquals(Collections.singleton(new SimpleGameScore(0, 0)), result.getMatches());
+    }
+
+    @Test
+    public void should_return_gamte_status_after_tow_scores() {
+        //Given
+        TennisGame tennisGame = new TennisGame();
+        //When
+        tennisGame.score(PlayerID.FIRST_PLAYER);
+        tennisGame.score(PlayerID.SECOND_PLAYER);
+        GameStatus result = tennisGame.getGameStatus();
+        //Then
+        List<SimpleGameScore> expectedStatus = new ArrayList<>();
+        expectedStatus.add(new SimpleGameScore(15, 0));
+        expectedStatus.add(new SimpleGameScore(15,15));
+        Assertions.assertIterableEquals(expectedStatus, result.getStatus());
+
+        List<SimpleGameScore> expectedScore = new ArrayList<>();
+        expectedScore.add(new SimpleGameScore(0, 0));
+        expectedScore.add(new SimpleGameScore(0, 0));
+        Assertions.assertIterableEquals(expectedScore, result.getScore());
+
+        List<SimpleGameScore> expectedMatch = new ArrayList<>();
+        expectedMatch.add(new SimpleGameScore(0, 0));
+        expectedMatch.add(new SimpleGameScore(0, 0));
+        Assertions.assertIterableEquals(expectedMatch, result.getMatches());
     }
 }
