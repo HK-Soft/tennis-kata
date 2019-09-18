77f3d26c1a6c7a5b359d1859342617be10bc2bb5
hadj naasse abdeldjalil <hn.abdeldjalil@gmail.com>
2019-09-18 14:38:20 +0200

    test: set score when passing a win tiebreak game

--- src/test/java/kata/tennis/SetScoreTest.java
+++ src/test/java/kata/tennis/SetScoreTest.java
@@ -3,6 +3,7 @@ package kata.tennis;
 import kata.tennis.scores.GameScoreType;
 import kata.tennis.scores.SetScore;
 import kata.tennis.scores.StandardGameScore;
+import kata.tennis.scores.TiebreakGameScore;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
@@ -18,5 +19,17 @@ public class SetScoreTest {
         SetScore result = tennisGame.nextSetScore(winStandardGameScore, currentSetScore);
         //Then
         Assertions.assertEquals(new SetScore(3, 5), result);
+    }
+
+    @Test
+    public void should_return_update_set_score_when_player_is_win_tiebreak_game() {
+        //Given
+        TennisGame tennisGame = new TennisGame();
+        SetScore currentSetScore = new SetScore(3, 4);
+        TiebreakGameScore witTiebreakGameScore = new TiebreakGameScore(7, 5);
+        //When
+        SetScore result = tennisGame.nextSetScore(witTiebreakGameScore, currentSetScore);
+        //Then
+        Assertions.assertEquals(new SetScore(4, 4), result);
     }
 }
