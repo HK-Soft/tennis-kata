18bcf30b7ec41ab7e03dad5dd667c4f780cfb8f3
hadj naasse abdeldjalil <hn.abdeldjalil@gmail.com>
2019-09-18 10:16:10 +0200

    implementation: both player score is less then 40

--- src/main/java/kata/tennis/TennisGame.java
+++ src/main/java/kata/tennis/TennisGame.java
@@ -7,7 +7,22 @@ public class TennisGame {
     private static final Logger LOGGER = Logger.getLogger(TennisGame.class.getName());
 
     public StandardGameScore nextStandardGameScore(PlayerID player, StandardGameScore currentScore) {
-        return null;
+        StandardGameScore result = currentScore.copy();
+        if (
+            (player == PlayerID.FIRST_PLAYER) &&
+            (currentScore.getFirstPlayerScore().ordinal() >= GameScoreType.LOVE.ordinal()) &&
+            (currentScore.getFirstPlayerScore().ordinal() < GameScoreType.FORTY.ordinal())
+        ) {
+            result.setFirstPlayerScore(GameScoreType.values()[currentScore.getFirstPlayerScore().ordinal() + 1]);
+        }
+        if (
+            (player == PlayerID.SECOND_PLAYER) &&
+            (currentScore.getFirstPlayerScore().ordinal() >= GameScoreType.LOVE.ordinal()) &&
+            (currentScore.getFirstPlayerScore().ordinal() < GameScoreType.FORTY.ordinal())
+        ) {
+            result.setSecondPlayerScore(GameScoreType.values()[currentScore.getSecondPlayerScore().ordinal() + 1]);
+        }
+        return result;
     }
 
     public static void main(String[] args) {
